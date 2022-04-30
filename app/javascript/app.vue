2f05bc17c092d91e6bd622507e5c011f0db52c90
masanarih0ri <masanarihoridevelop@gmail.com>
2022-04-30 17:36:43 +0900

    use vue on rails

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <p>{{ message }}</p>
   </div>
 </template>
@@ -8,7 +8,7 @@
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue.js!"
     }
   }
 }
