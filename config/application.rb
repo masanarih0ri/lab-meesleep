406b6bf04d8f760e1527aa5f7eaf3c70fe636e9d
masanarih0ri <masanarihoridevelop@gmail.com>
2022-05-04 15:21:37 +0900

    add tailwind3

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module LabMeesleep
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.assets = false
+    config.generators.helper = false
   end
 end
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
